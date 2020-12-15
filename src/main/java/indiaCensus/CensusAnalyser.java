ef3bf9d452d23e6f26b2900a45978ba95f061076
chetan <zambare.c123@gmail.com>
2020-12-15 21:37:19 +0530

    [Chetan] Added:

---
+++ src/main/java/indiaCensus/CensusAnalyser.java
@@ -0,0 +1,35 @@
+package indiaCensus;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
+public class CensusAnalyser {
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder(reader).withType(IndiaCensusCSV.class).withIgnoreLeadingWhiteSpace(true).build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+            int namOfEnteries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+            return namOfEnteries;
+        }
+        catch (RuntimeException e){
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.DATA_IMPROPER);
+        }
+        catch (IOException e) {
+            if(!(csvFilePath.matches("^.+\\.csv$"))) {
+                throw new CensusAnalyserException("Invalid File", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+            }
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+
+    }
+
+}
---
+++ src/test/java/indiaCensus/CensusAnalyserTest.java
@@ -0,0 +1,69 @@
+package indiaCensus;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CensusAnalyserTest {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_FILE_TYPE = "./src/main/resources/IndiaStateCensusData.txt";
+    private static final String DELIMINATOR_MISS = "D:\\eclipse-java-2020-09-R-win32-x86_64\\Week3,Workspace Eclipse\\SensusIndia\\src\\test\\resources\\IndiaStateCensusDataDelimininatorMiss.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+    @Test
+    public void givenIndiaCensusData_WithWrongFileType_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE,e.type);
+        }
+
+    }
+    @Test
+    public void givenIndiaCensusData_WithDeliminatorMiss_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(DELIMINATOR_MISS);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DATA_IMPROPER,e.type);
+        }
+    }
+    @Test
+    public void givenIndiaCensusData_WithHeaderrMiss_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(DELIMINATOR_MISS);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DATA_IMPROPER,e.type);
+        }
+    }
+
+}
